a039bcd22ca779c3ab2906c8a657880551cc4968
Kaisei Sunaga <skkbyssk@gmail.com>
2020-12-09 00:25:31 +0900

    Remove unused project

--- streamar-grpc/proto.sh
+++
@@ -1,5 +0,0 @@
-BASEDIR=$(dirname "$0")
-SRC_DIR="$BASEDIR"
-IOS_DST_DIR="$BASEDIR/../streamar-ios/streamar-ios/Proto"
-
-protoc --swift_out=$IOS_DST_DIR --swiftgrpc_out=$IOS_DST_DIR "$SRC_DIR/channel.proto"
